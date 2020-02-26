5d58e0c8c68827aafcf02e08055f6e94db6f2cb7
Udit Gupta <luctivud@Udits-MacBook-Air.local>
2020-02-26 17:58:15 +0530

    day 2

---
+++ cf-1311-C-perform-the-combo.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef unsigned long long int llu;
+
+int main(){
+    llu t; cin>>t;
+    while(t--){
+        llu sz, n;
+        cin>>sz>>n;
+        string s;
+        cin>>s;
+        llu arr[26]={0};
+        llu st[sz]={0};
+        str[n-1] = 1;
+        for(llu i=0;i<n; i++){
+            llu x; cin>>x;
+
+        }
+        cout<<endl;
+    }
+    return 0;
+}
