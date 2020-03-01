4116ea2884a04e70d6633a69ed4909b3feb08c2e
Udit Gupta <luctivud@Udits-MacBook-Air.local>
2020-03-01 18:34:42 +0530

    hh

--- 00-MY-TEMPLATE.cpp
+++ 00-MY-TEMPLATE.cpp
@@ -11,13 +11,12 @@ using namespace std;
 typedef unsigned long long int llu;
 typedef long long int ll;
 
+#define TESTCASES() int TestCases; cin>>TestCases; while(TestCases--)
 #define pb push_back
 #define MOD 1000000007
-
-#define TEST() int t; cin>>t; while(t--)
 #define FORI(start,end) for(llu i=start; i<end; i++)
 
 int main(){
-  TEST(){}
+  TESTCASES(){}
   return 0;
 }
