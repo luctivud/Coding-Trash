18d75c886d5696f316629281323e5c2a2f628636
Udit Gupta <luctivud@Udits-MacBook-Air.local>
2020-03-02 06:59:12 +0530

    updated template

--- 00-MY-TEMPLATE.cpp
+++ 00-MY-TEMPLATE.cpp
@@ -1,22 +1,69 @@
-/*```````````````````````````````````````
-    **      **  *******  * *  ******
-    **      **  **   **  ***    **
-    **      **  **    *  * *    **
-    ******* **  *******  * *    **
-`````````````````````````````````````````*/
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+  Udit Gupta @luctivud  神 | LIGHT | GREED | CIPHER | XAYN
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                **      **  *******  * *  ******
+                **      **  **   **  ***    **
+                **      **  **    *  * *    **
+                ******* **  *******  * *    **
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  WORSHIPPER OF GREED | НАРУШИТЕЛЬ ЗАКОНА | U DON'T KNOW ME
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+/********   All Required Header Files ********/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+/********   //All Required Header Files ********/
+
 using namespace std;
 
 typedef unsigned long long int llu;
 typedef long long int ll;
+typedef long double ldo;
+#define FASTIO                              ios_base::sync_with_stdio(false);
+#define TESTCASES()                         llu TestCases; cin>>TestCases; while(TestCases--)
+#define mp                                  make_pair
+#define pb                                  push_back
+#define MOD                                 1000000007
+#define mems(arr,val)                       memset((arr), val, sizeof((arr)))
+#define loop(iter, start, end, incr)        for (int iter=start ; i<end ; i+=incr)
+#define rloop(iter, start, end, decr)       for (int iter=start ; i>=end ; i-=decr)
+#define FOREACH(it, ___vect)                for (auto it = ___vect.begin(); it != ___vect.end(); it++)
 
-#define TESTCASES() llu TestCases; cin>>TestCases; while(TestCases--)
-#define pb push_back
-#define MOD 1000000007
-#define FORI(start,end) for(llu i=start; i<end; i++)
+void solve(){
+
+    return;
+}
 
 int main(){
-  TESTCASES(){}
-  return 0;
+    FASTIO
+    #ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+    TESTCASES()
+        solve();
+
+    return 0;
 }
