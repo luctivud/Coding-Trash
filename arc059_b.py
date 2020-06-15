f8a413dba3f40e1700b8bc849641d3c179b44da4
luctivud <luctivud@gmail.com>
2020-06-15 21:59:13 +0530

    prc

---
+++ arc059_b.py
@@ -0,0 +1,48 @@
+'''     ##     ##  #######  # #  ######
+        ##     ##  ##   ##  ###    ##
+        ##     ##  ##    #  # #    ##
+        #########  #######  # #    ##    '''
+
+import sys
+import math
+# sys.setrecursionlimit(10**6)
+
+def get_ints(): return map(int, input().strip().split())
+def get_list(): return list(get_ints())
+
+def printspx(*args): return print(*args, end="")
+def printsp(*args): return print(*args, end=" ")
+def printchk(*args): return print(*args, end=" \ ")
+
+MODPRIME = int(1e9+7); BABYMODPR = 998244353;
+
+ONLINE_JUDGE = 1
+if not ONLINE_JUDGE:
+    sys.stdin  = open("input.txt","r")
+    sys.stdout = open("output.txt","w")
+    sys.stderr = open("output.txt","w")
+
+# for _testcases_ in range(int(input())):
+s = input()
+a = b = -1
+for i in range(len(s)-1):
+    if s[i] == s[i+1]:
+        a, b = i+1, i+2
+        break
+    elif i != len(s) - 2 and s[i] == s[i+2]:
+        a, b = i+1, i+3
+        break
+print(a, b)
+
+
+
+
+'''
+>>> COMMENT THE STDIN!! CHANGE ONLINE JUDGE !!
+THE LOGIC AND APPROACH IS BY ME @luctivud ( UDIT GUPTA )
+SOME PARTS OF THE CODE HAS BEEN TAKEN FROM WEBSITES LIKE::
+(I Own the code if no link is provided here or I may have missed mentioning it)
+DO NOT PLAGIARISE.
+TESTCASES:
+>>> COMMENT THE STDIN!! CHANGE ONLINE JUDGE !!
+'''
---
+++ cf-435-b.py
@@ -0,0 +1,51 @@
+'''     ##     ##  #######  # #  ######
+        ##     ##  ##   ##  ###    ##
+        ##     ##  ##    #  # #    ##
+        #########  #######  # #    ##    '''
+
+import sys
+import math
+# sys.setrecursionlimit(10**6)
+
+def get_ints(): return map(int, input().strip().split())
+def get_list(): return list(get_ints())
+
+def printspx(*args): return print(*args, end="")
+def printsp(*args): return print(*args, end=" ")
+def printchk(*args): return print(*args, end=" \ ")
+
+MODPRIME = int(1e9+7); BABYMODPR = 998244353;
+
+ONLINE_JUDGE = 1
+if not ONLINE_JUDGE:
+    sys.stdin  = open("input.txt","r")
+    sys.stdout = open("output.txt","w")
+    sys.stderr = open("output.txt","w")
+
+# for _testcases_ in range(int(input())):
+s, k = input().split()
+s = list(s)
+k = int(k)
+i = 0
+while i < len(s) and k:
+    mx = mxp = -1
+    for j in range(min(k+1, len(s)-i)):
+        if int(s[i + j]) > mx:
+            mx = int(s[i+j])
+            mxp = i + j
+    if i != mxp:
+        k -= (mxp - i)
+        s.pop(mxp)
+        s.insert(i, str(mx))
+    # print(s, k)
+    i += 1
+print("".join(s))
+'''
+>>> COMMENT THE STDIN!! CHANGE ONLINE JUDGE !!
+THE LOGIC AND APPROACH IS BY ME @luctivud ( UDIT GUPTA )
+SOME PARTS OF THE CODE HAS BEEN TAKEN FROM WEBSITES LIKE::
+(I Own the code if no link is provided here or I may have missed mentioning it)
+DO NOT PLAGIARISE.
+TESTCASES:
+>>> COMMENT THE STDIN!! CHANGE ONLINE JUDGE !!
+'''
