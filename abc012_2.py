4111b8e6bf711d364cac9837ec7f6d7f5d6a4c66
luctivud <luctivud@gmail.com>
2020-06-17 16:03:06 +0530

    prc

---
+++ abc012_2.py
@@ -0,0 +1,32 @@
+#                           जय श्री राम
+
+import sys; import math; from collections import *
+# sys.setrecursionlimit(10**6)
+
+def get_ints(): return map(int, input().split())
+def get_list(): return list(get_ints())
+def printspx(*args): return print(*args, end="")
+def printsp(*args): return print(*args, end=" ")
+
+UGLYMOD = int(1e9+7); SEXYMOD = 998244353; MAXN = int(1e5)
+
+# sys.stdin  = open("input.txt","r"); sys.stdout = open("output.txt","w")
+
+# for _testcases_ in range(int(input())):
+h = m = s = 0
+n = int(input())
+s = str(n % 60)
+n //= 60
+m = str(n % 60)
+n //= 60
+h = str(n % 60)
+print('{}:{}:{}'.format(h.zfill(2), m.zfill(2), s.zfill(2)))
+
+
+'''
+>>> COMMENT THE STDIN!! CHANGE ONLINE JUDGE !!
+THE LOGIC AND APPROACH IS MINE @luctivud ( UDIT GUPTA )
+Link may be copy-pasted here if it's taken from other source.
+DO NOT PLAGIARISE.
+>>> COMMENT THE STDIN!! CHANGE ONLINE JUDGE !!
+'''
