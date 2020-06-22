caa79783d687452c32de09258af44f056fb2860b
luctivud <luctivud@gmail.com>
2020-06-22 20:22:11 +0530

    prc

---
+++ arc066_a.py
@@ -0,0 +1,39 @@
+#                          JAI SHREE RAM
+
+import math; from collections import *
+import sys; from functools import reduce
+
+def get_ints(): return map(int, input().strip().split())
+def get_list(): return list(get_ints())
+def getlistofstring(): return list(input().strip().split())
+def printxsp(*args): return print(*args, end="")
+def printsp(*args): return print(*args, end=" ")
+
+UGLYMOD = int(1e9)+7; SEXYMOD = 998244353; MAXN = int(1e5)
+
+# sys.setrecursionlimit(10**6)
+sys.stdin=open("input.txt","r");sys.stdout=open("output.txt","w")
+
+# for _testcases_ in range(int(input())):
+n = int(input())
+d = Counter(get_list())
+flag = False
+if n & 1: flag = True
+ans = 1
+for k, v in d.items():
+    if v & 1:
+        if not flag:
+            ans = 0
+            break
+        else:
+            flag = False
+    else:
+        ans *= 2
+print(ans)
+'''
+>>> COMMENT THE STDIN!! CHANGE ONLINE JUDGE !!
+THE LOGIC AND APPROACH IS MINE @luctivud ( UDIT GUPTA )
+Link may be copy-pasted here if it's taken from other source.
+DO NOT PLAGIARISE.
+>>> COMMENT THE STDIN!! CHANGE ONLINE JUDGE !!
+'''
