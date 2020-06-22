f8c305ce262222d847878537f312877abf0c490e
luctivud <luctivud@gmail.com>
2020-06-22 09:35:13 +0530

    test gen file added

---
+++ test-generation-file.py
@@ -0,0 +1,15 @@
+#                           जय श्री राम
+
+sys.stdout = open("input.txt","w")
+
+import sys; import math; from collections import *; import random
+# sys.setrecursionlimit(10**6)
+
+def get_ints(): return map(int, input().split())
+def get_list(): return list(get_ints())
+def printspx(*args): return print(*args, end="")
+def printsp(*args): return print(*args, end=" ")
+
+UGLYMOD = int(1e9+7); SEXYMOD = 998244353; MAXN = int(1e5)
+
+
