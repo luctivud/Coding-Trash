4e1ab150fc753a99fc430570885ca994ceb1d690
luctivud <luctivud@gmail.com>
2020-07-05 21:29:55 +0530

    cf-prc

---
+++ cf-472-a.py
@@ -0,0 +1,31 @@
+#                          JAI SHREE RAM
+
+import math; from collections import *
+import sys; from functools import reduce
+
+# sys.setrecursionlimit(10**6)
+def get_ints(): return map(int, input().strip().split())
+def get_list(): return list(get_ints())
+def get_string(): return list(input().strip().split())
+def printxsp(*args): return print(*args, end="")
+def printsp(*args): return print(*args, end=" ")
+
+UGLYMOD = int(1e9)+7; SEXYMOD = 998244353; MAXN = int(1e5)
+
+# sys.stdin=open("input.txt","r");sys.stdout=open("output.txt","w")
+
+# for _testcases_ in range(int(input())):
+n = int(input())
+if n & 1:
+    print(9, n-9)
+else:
+    print(8, n-8)
+
+
+'''
+>>> COMMENT THE STDIN!! CHANGE ONLINE JUDGE !!
+THE LOGIC AND APPROACH IS MINE @luctivud ( UDIT GUPTA )
+Link may be copy-pasted here if it's taken from other source.
+DO NOT PLAGIARISE.
+>>> COMMENT THE STDIN!! CHANGE ONLINE JUDGE !!
+'''
---
+++ cf-750-a.py
@@ -0,0 +1,33 @@
+#                          JAI SHREE RAM
+
+import math; from collections import *
+import sys; from functools import reduce
+
+# sys.setrecursionlimit(10**6)
+def get_ints(): return map(int, input().strip().split())
+def get_list(): return list(get_ints())
+def get_string(): return list(input().strip().split())
+def printxsp(*args): return print(*args, end="")
+def printsp(*args): return print(*args, end=" ")
+
+UGLYMOD = int(1e9)+7; SEXYMOD = 998244353; MAXN = int(1e5)
+
+sys.stdin=open("input.txt","r");sys.stdout=open("output.txt","w")
+
+# for _testcases_ in range(int(input())):
+n, k = get_ints()
+time = i = 0
+while time <= 240 - k:
+    time += 5 * i
+    i += 1
+    print(time)
+print(i-2)
+
+
+'''
+>>> COMMENT THE STDIN!! CHANGE ONLINE JUDGE !!
+THE LOGIC AND APPROACH IS MINE @luctivud ( UDIT GUPTA )
+Link may be copy-pasted here if it's taken from other source.
+DO NOT PLAGIARISE.
+>>> COMMENT THE STDIN!! CHANGE ONLINE JUDGE !!
+'''
