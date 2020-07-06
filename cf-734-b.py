25cf53c29b4e33078a5731b7942a2e0febdce917
luctivud <luctivud@gmail.com>
2020-07-06 10:07:37 +0530

    cf-prc-set-2

---
+++ cf-734-b.py
@@ -0,0 +1,32 @@
+#                          JAI SHREE RAM
+
+import math; from collections import *
+import sys; from functools import reduce
+
+# sys.setrecursionlimit(10**6)
+def get_ints(): return map(int, input().strip().split())
+def get_list(): return list(get_ints())
+def get_string(): return list(input().strip().split())
+def printxsp(*args): return print(*args, end="")
+def printsp(*args): return print(*args, end=" ")
+
+UGLYMOD = int(1e9)+7; SEXYMOD = 998244353; MAXN = int(1e5)
+
+# sys.stdin=open("input.txt","r");sys.stdout=open("output.txt","w")
+
+# for _testcases_ in range(int(input())):
+k2, k3, k5, k6 = get_ints()
+a = min(k2, k5, k6)
+k2 -= a
+b = min(k3, k2)
+print(256*a+32*b)
+
+
+
+'''
+>>> COMMENT THE STDIN!! CHANGE ONLINE JUDGE !!
+THE LOGIC AND APPROACH IS MINE @luctivud ( UDIT GUPTA )
+Link may be copy-pasted here if it's taken from other source.
+DO NOT PLAGIARISE.
+>>> COMMENT THE STDIN!! CHANGE ONLINE JUDGE !!
+'''
---
+++ cf-9-a.py
@@ -0,0 +1,30 @@
+#                          JAI SHREE RAM
+
+import math; from collections import *
+import sys; from functools import reduce
+
+# sys.setrecursionlimit(10**6)
+def get_ints(): return map(int, input().strip().split())
+def get_list(): return list(get_ints())
+def get_string(): return list(input().strip().split())
+def printxsp(*args): return print(*args, end="")
+def printsp(*args): return print(*args, end=" ")
+
+UGLYMOD = int(1e9)+7; SEXYMOD = 998244353; MAXN = int(1e5)
+
+# sys.stdin=open("input.txt","r");sys.stdout=open("output.txt","w")
+
+# for _testcases_ in range(int(input())):
+a, b = get_ints()
+p = 6 - max(a, b) + 1
+q = 6
+p, q = p//math.gcd(p, q), q//math.gcd(p, q)
+print("{}/{}".format(p, q))
+
+'''
+>>> COMMENT THE STDIN!! CHANGE ONLINE JUDGE !!
+THE LOGIC AND APPROACH IS MINE @luctivud ( UDIT GUPTA )
+Link may be copy-pasted here if it's taken from other source.
+DO NOT PLAGIARISE.
+>>> COMMENT THE STDIN!! CHANGE ONLINE JUDGE !!
+'''
