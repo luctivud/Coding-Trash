53c51822992ad9ee15f9c77b167d5040cc072062
luctivud <luctivud@gmail.com>
2020-07-12 13:36:41 +0530

    google kickstart round d

---
+++ kickstart-b.cpp
@@ -0,0 +1,139 @@
+/*     <<<  J A I ~ S H R E E ~ R A M  >>>     */
+
+// Title: kickstart-b.cpp
+// dated: 12-07-2020 10:54:00
+// Creator & Template by : Udit Gupta @luctivud
+
+
+// #include "bits/stdc++.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+const long long int MOD_JOHAN {1000000007};
+const long long int MOD_LIGHT {998244353};
+const long long int MAXN_EYEPATCH = 1e6 + 1LL;
+const long long int MAXN_FULLMETAL = 501LL;
+
+typedef long long int lld;
+typedef unsigned long long int llu;
+typedef vector<int> v_int;
+typedef vector<lld> v_lld;
+typedef pair<int, int> p_int;
+typedef pair<lld, lld> p_lld;
+
+
+#define                   GREED_FOR_SPEED   ios_base::sync_with_stdio(false); cin.tie(0)
+#define             TESTCASES_ARE_THERE()   _SC4N(Test0xcs); TotalT35tC4ses = Test0xcs; while(Test0xcs--)
+#define                  mems(A77AY,V4LU)   memset((A77AY), V4LU, sizeof((A77AY)))
+#define   FOR4WD(IT7T, ST47T, E9xD, St3P)   for(auto IT7T = ST47T; IT7T < E9xD; IT7T += St3P)
+#define   FOR8WD(IT7T, ST47T, E9xD, St3P)   for(auto IT7T = ST47T; IT7T > E9xD; IT7T -= St3P)
+#define  FOR4NGE(IT7T, ST47T, E9xD, St3P)   for(auto IT7T = ST47T;((St3P<0) ? IT7T>E9xD : IT7T<E9xD); IT7T += St3P)
+#define                              endl   "\n"
+#define            FORE4CH(V3CT07_, IT7T)   for(auto IT7T = V3CT07_.begin(); IT7T != V3CT07_.end(); IT7T++)
+#define                  _INP47T(V3CT07_)   for(auto &V3CT07____IT7T : V3CT07_) _SC4N(V3CT07____IT7T)
+#define                                PI   3.1415926535897932384626433832795
+#define                          INF1N1TY   1000000000000000005LL;
+#define                    A47L(V3CT07_t)   V3CT07_t.begin(), V3CT07_t.end()
+#define             L3N(V3CT07_x, Typ30F)   (Typ30F) V3CT07_x.size()
+#define                            un_map   unordered_map
+#define                            un_set   unordered_set
+
+
+template<typename T> void _SC4N(T &x) { 
+    x = 0; bool neg = 0; 
+    register T c = getchar();
+    if (c == '-') neg = 1, c = getchar();
+    while ((c < 48) || (c > 57)) c = getchar();
+    for ( ; c < 48||c > 57 ; c = getchar());
+    for ( ; c > 47 && c < 58; c = getchar() ) x= (x << 3) + ( x << 1 ) + ( c & 15 );
+    if (neg) x *= -1;
+}
+
+
+void printYesNo(bool Expr, int YNType = 1, string FirstChoice = "Ud", string SecondChoice = "Ud") { 
+    if (YNType == 1) puts(Expr ? "YES" : "NO");
+    else if (YNType == 2) puts(Expr ? "Yes" : "No");
+    else if (YNType == 3) puts(Expr ? "yes" : "no");
+    else if (YNType == 4) cout << (Expr ? FirstChoice : SecondChoice);
+}
+
+
+
+void solveEach(lld TestCaseNumber = 1) {
+    lld n; _SC4N(n);
+    v_lld arr(n); _INP47T(arr);
+    lld ans = 0;
+    lld incr = 0, decr = 0;
+    
+    FOR4NGE(i, 1, n, 1) {
+        if (arr[i] < arr[i-1]) {
+            if (incr != 0) {
+                ans += incr/4;
+                decr = 0;
+                incr = 0;
+            }
+            decr++;
+        } else if (arr[i] > arr[i-1]) {
+            if (decr != 0) {
+                ans += decr/4;
+                incr = 0;
+                decr = 0;
+            }
+            incr++;
+        }
+        // cout << incr << " " << decr << " " << rem << ", ";
+    }
+    
+
+    ans += incr / 4;
+    ans += decr / 4;
+
+    cout << "Case #" << TestCaseNumber << ": " << ans << "\n"; 
+    return;
+}
+
+
+signed main() {
+
+    // GREED_FOR_SPEED;
+
+    lld Test0xcs = 0, TotalT35tC4ses = 1;
+
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif 
+
+    TESTCASES_ARE_THERE()
+        solveEach(TotalT35tC4ses - Test0xcs);
+    return 0;
+}
+
+/*
+THE LOGIC AND APPROACH IS MINE ( UDIT GUPTA )
+SOME PARTS OF THE CODE MAY HAVE BEEN TAKEN FROM WEBSITES LIKE::
+(I Own the code if no link is provided here or I may have missed mentioning it)ch|=spacelo &=_up change case
+*/
---
+++ kickstart-c-subtask-1-specific-a2.py
@@ -0,0 +1,65 @@
+'''     J A I ~ S H R E E ~ R A M     '''
+
+# Title: kickstart-c-subtask-1-specific.py
+# dated: 12-07-2020 12:58:15
+# Creator & Template by : Udit Gupta @luctivud
+
+
+import math; from collections import *
+import sys; from functools import reduce
+
+# sys.setrecursionlimit(10**6)
+
+def get_ints(): return map(int, input().strip().split())
+def get_list(): return list(get_ints())
+def get_string(): return list(input().strip().split())
+def printxsp(*args): return print(*args, end="")
+def printsp(*args): return print(*args, end=" ")
+
+DIRECTIONS = [[0, 1], [0, -1], [1, 0], [1, -1]] #up, down, right, left
+NEIGHBOURS = [(i, j) for i in range(-1, 2) for j in range(-1, 2) if (i!=0 or j!=0)]
+
+OrdUnicode_a = ord('a'); OrdUnicode_A = ord('A')
+CAPS_ALPHABETS = {chr(i+OrdUnicode_A) : i for i in range(26)}
+SMOL_ALPHABETS = {chr(i+OrdUnicode_a) : i for i in range(26)}
+
+MOD_JOHAN = int(1e9)+7; MOD_LIGHT = 998244353; INFINITY = float('inf')
+MAXN_EYEPATCH = int(1e5)+1; MAXN_FULLMETAL = 501
+
+# sys.stdin=open("input.txt","r");sys.stdout=open("output.txt","w")
+
+for _testcases_ in range(int(input())): 
+    printsp("Case #{}:".format(_testcases_+1))
+    n, a, b = get_ints()
+    parent = [0, 0] + get_list()
+    
+    ans = 0
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            se = set()
+            alice = i
+            bob = j
+            astep = 0
+            bstep = 0
+            while alice != 0:
+                if (astep % a) == 0:
+                    se.add(alice)
+                alice = parent[alice]
+                astep+=1
+            while bob != 0:
+                if (bstep % b) == 0:
+                    se.add(bob)
+                bob = parent[bob]
+                bstep += 1
+            ans += len(se)
+    print(ans / (n ** 2))
+
+
+
+
+
+'''
+>>> COMMENT THE STDIN!! CHANGE ONLINE JUDGE !!
+THE LOGIC AND APPROACH IS MINE ( UDIT GUPTA )
+Link may be copy-pasted here if it's taken from other source.
+'''
